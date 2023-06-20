d142b6c97838fe2dbb698057127f9e1c55ad3cf4
Darshana Prahath <90643832+darshana-prabath@users.noreply.github.com>
2023-06-20 10:41:42 +0530

    qr code genarator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{ SafeValue} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'qr-code-genarator';
+  title='qr code generator';
+  qrdata='hellow1234';
+  qrcodedownloadlink: SafeValue = '';
+
+onchange(url: SafeValue){
+ this.qrcodedownloadlink = url;
+ 
+}
+
+
 }
